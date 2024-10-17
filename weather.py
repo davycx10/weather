d9900cdf548352e572093343dbfc1e7ee513be65
Dave Israël <dave.israel@oskey.io>
2024-10-17 10:09:44 +0200

    fix/link_api

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 
 #demander les donner de l'API
-response = requests.get() 
+response = requests.get('https://public-api.meteofrance.fr/public/arpege/1.0', params={'stations': 'PARIS'}) 
 
 #Parser les donnée JSON
-
+data = response
