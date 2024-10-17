52cbae2b1b37b205e260f7ed3f271fd98dc71dd7
Dave Israël <dave.israel@oskey.io>
2024-10-17 12:09:39 +0200

    feat/add_temps/description

--- weather.py
+++ weather.py
@@ -1,7 +1,24 @@
 import requests
 
-#demander les donner de l'API
-response = requests.get('https://public-api.meteofrance.fr/public/arpege/1.0', params={'stations': 'PARIS'}) 
+city_name = input("mettre le nom de la ville souhaité: ")
 
-#Parser les donnée JSON
-data = response
+langage = "fr" # la sortie de la langue est en français
+key = "288dadf7b5d8930928ac12107a4682e8"
+api_link = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={key}&lang={langage}"
+
+# Récupérer les données JSON
+response = requests.get(api_link)
+data = response.json()
+
+if 'weather' in data and len(data['weather']) > 0:
+    description = data['weather'][0]['description']
+    print("Description :", description)
+    temp_kelvin = data['main']['temp']
+    temp_celsius = temp_kelvin - 273.15  # Conversion de Kelvin à Celsius
+    print("Température :", round(temp_celsius, ),"°C")  # Affiche la température 
+else:
+    print("Aucune donnée météo disponible.")
+
+# Afficher la réponse complète si nécessaire
+#print(data)
+
